ef4b21d480c773445ba7d8a54beb8dc641cd548f
Hashmi, Yasub <yas.hashmi@gmail.com>
2020-08-12 11:09:13 +0530

    Initial Commit: Requirement 1

---
+++ CSharp/StringCalculator.Logic.Tests/StringCalculator1Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator.Logic.Tests
+{
+    public class StringCalculator1Test
+    {
+        [Test]
+        public void whenMoreThan2NumbersAreUsedThenExceptionIsThrown()
+        {
+            Assert.Throws(typeof(ArgumentException), () => StringCalculator1.Add("1,2,3"));
+        }
+
+        [Test]
+        public void when2NumbersAreUsedThenNoExceptionIsThrown()
+        {
+            Assert.DoesNotThrow(() => StringCalculator1.Add("1,2"));
+        }
+
+        [Test]
+        public void whenNonNumberIsUsedThenExceptionIsThrown()
+        {
+            Assert.Throws(typeof(FormatException), () => StringCalculator1.Add("1,x"));
+        }
+    }
+}
---
+++ CSharp/StringCalculator.Logic/StringCalculator1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringCalculator.Logic
+{
+    public class StringCalculator1
+    {
+        public static void Add(string numbers)
+        {
+            var numbersArray = numbers.Split(',');
+            if (numbersArray.Length > 2)
+            {
+                throw new ArgumentException();
+            }
+            foreach(var number in numbersArray)
+            {
+                Int16.Parse(number);
+            }
+        }
+    }
+}
